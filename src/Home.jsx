d415e1d6f4464a96d19266e3bbd5560b94df9c01
dilshan-ah <dilshaneffendi1123@gmail.com>
2023-11-20 09:11:34 +0600

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { InfinitySpin } from "react-loader-spinner";
+import { Link } from "react-router-dom";
+import { DataContext } from "./Context/Context";
+import Card from "./components/Card";
+import Header from "./components/Header";
+
+const Home = () => {
+  const { data, loading } = useContext(DataContext);
+
+  return (
+    <div className="mt-10">
+     <Header/>
+      <div className="grid grid-cols-3 gap-10">
+        {loading ? (
+          <div className="flex items-center justify-center w-100 h-screen">
+            <InfinitySpin />
+          </div>
+        ) : (
+          data?.products?.map((each) => (
+            <Link to={`product/${each?.id}`} key={each.id}>
+              {" "}
+              <Card item={each} />
+            </Link>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import Context from "./Context/Context.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Context>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Context>
+  </React.StrictMode>
+);
